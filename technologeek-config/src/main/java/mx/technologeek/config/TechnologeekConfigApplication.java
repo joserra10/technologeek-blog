0f1630b819a9bc0fe7f73ec212c1787fd1f0c3df
joserra10 <jofo109@gmail.com>
2020-11-10 19:32:57 -0600

    Sube proyecto de configuracion

---
+++ technologeek-config/src/main/java/mx/technologeek/config/TechnologeekConfigApplication.java
@@ -0,0 +1,15 @@
+package mx.technologeek.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class TechnologeekConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnologeekConfigApplication.class, args);
+    }
+
+}
